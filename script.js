2be00e3dfd2fcf8108f92dc0a3743ded328995ad
nesrine khouas <nesrine.khouas230@gmail.com>
2023-10-27 21:51:43 +0100

    commit file

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let input = document.querySelector('.entered-list');
 let addBtn = document.querySelector('.add-list');
 let tasks = document.querySelector('.tasks');
 
+// Load tasks from local storage when the page loads
+window.addEventListener('load', () => {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+        tasks.innerHTML = savedTasks;
+    }
+});
+
 input.addEventListener('keyup', () => {
     if (input.value.trim() !== '') {
         addBtn.classList.add('active');
@@ -12,26 +20,40 @@ input.addEventListener('keyup', () => {
 
 addBtn.addEventListener('click', () => {
     if (input.value.trim() !== '') {
-        let newItem = document.createElement('div');
-        newItem.classList.add('item');
-        newItem.innerHTML = `
-            <p>${input.value}</p>
-          <div class="item-btn">
-            <i class="fa-solid fa-list-check"></i>
-            <i class="fa-solid fa-trash"></i>
-          </div>
-        `;
-        tasks.appendChild(newItem);
+        createTaskElement(input.value);
+
+        // Save the updated tasks to local storage
+        localStorage.setItem('tasks', tasks.innerHTML);
+
         input.value = '';
-    } 
+    }
 });
 
 tasks.addEventListener('click', (doneanddelete) => {
     if (doneanddelete.target.classList.contains('fa-trash')) {
-      doneanddelete.target.parentElement.parentElement.remove();
-        
+        doneanddelete.target.parentElement.parentElement.remove();
+
+        // Save the updated tasks to local storage
+        localStorage.setItem('tasks', tasks.innerHTML);
     } else if (doneanddelete.target.classList.contains('fa-list-check')) {
-      doneanddelete.target.parentElement.parentElement.classList.toggle('completed');
-        
+        doneanddelete.target.parentElement.parentElement.classList.toggle('completed');
+
+        // Save the updated tasks to local storage
+        localStorage.setItem('tasks', tasks.innerHTML);
     }
 });
+
+function createTaskElement(taskText) {
+    let newItem = document.createElement('div');
+    newItem.classList.add('item');
+    newItem.innerHTML = `
+        <p>${taskText}</p>
+        <div class="item-btn">
+            <i class="fa-solid fa-list-check"></i>
+            <i class="fa-solid fa-trash"></i>
+        </div>
+    `;
+    tasks.appendChild(newItem);
+}
+
+
